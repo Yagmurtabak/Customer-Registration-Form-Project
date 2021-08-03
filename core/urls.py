29c44748503ad64957f1b6500e66fac5a8eab030
Yagmurtabak <yagmurtabak@hotmail.com>
2021-08-03 10:17:30 +0300

    post renamed to core

--- core/urls.py
+++ core/urls.py
@@ -7,10 +7,10 @@ from core.views import register
 from django.contrib.auth import views as authViews
 
 urlpatterns = [
-     path('',customerListView.as_view(), name="list"),
-    path('detail/<int:pk>', customerDetailView.as_view(), name="detail"),
-    path('update/<int:pk>', customerUpdateView.as_view(), name="update"),
-    path('delete/<int:pk>',  customerDeleteView.as_view(),name="delete"),
+    path('',customerListView.as_view(), name="list"),
+    path('detail/<int:pk>/', customerDetailView.as_view(), name="detail"),
+    path('update/<int:pk>/', customerUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>/',  customerDeleteView.as_view(),name="delete"),
     path('create', customerCreateView.as_view(), name="create"),
     path('registerate', RegisterView.as_view(), name= "registerate"),
     path('register/', register, name="register"),
